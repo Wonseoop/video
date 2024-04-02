a6ba1eb47e8b7c97ddf146d95e04bcd569d2d123
원현섭 <123632194+Wonseoop@users.noreply.github.com>
2024-04-02 17:40:21 +0900

    Add files via upload

--- webcam.py
+++ webcam.py
@@ -1,44 +1,26 @@
 import cv2
 
-webcam = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('2.mp4')
+if cap.isOpened:
+    file_path = 'record.mp4'
+    fps = 25.40
+    fourcc = cv2.VideoWriter_fourcc(*'DIVX')  # 인코딩 포맷 문자
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    size = (int(width), int(height))  # 프레임 크기
 
-if not webcam.isOpened():
-    print("Could not open webcam")
-    exit()
-
-while webcam.isOpened():
-    status, frame = webcam.read()
-
-    if status:
-        cv2.imshow("test", frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-webcam.release()
-cv2.destroyAllWindows()
-
-
-# 모듈 불러오기: import 모듈 이름 - 이미지 사용을 위한 opencv,
-# 이미지 저장 파일명 사용을 위한 datetime
-
-#qimport cv2
-#import datetime
-
-#video_capture = cv2.VideoCapture(0)
-
-#while (True):
-
-#    grabbed, frame = video_capture.read()
-#    cv2.imshow('Original Video', frame)
-
- #   key = cv2.waitKey(1);
- #   if key == ord('q'):
- #       break
- #   elif key == ord('s'):
- #       file = datetime.datetime.now().strftime("%Y%m%d_%H%M%S%f") + '.jpg'
- #       cv2.imwrite(file, frame)
- #       print(file, ' saved')
-
-#video_capture.release()
-#cv2.destroyAllWindows()
+    out = cv2.VideoWriter(file_path, fourcc, fps, size)  # VideoWriter 객체 생성
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            cv2.imshow('camera-recording', frame)
+            out.write(frame)  # 파일 저장
+            if cv2.waitKey(int(1000 / fps)) != -1:
+                break
+        else:
+            print('no file!')
+            break
+    out.release()  # 파일 닫기
+else:
+    print("Can`t open camera!")
+cap.release()
